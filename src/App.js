da532591f590489509a197520e392022e53fb0d3
Moulya-Hegde <1dt22cs@dsatm.edu.in>
2024-11-15 19:41:20 +0530

    first day end

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Hellooooo</h2>
       <Router>
         <Routes>
           <Route path='/' element={<Login />}>
